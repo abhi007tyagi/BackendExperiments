c962ae1ccb8570b44063f1bc1e3c5872dd9f3494
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2016-03-18 09:23:52 +0530

    added request for permission for API23 for fine location

---
+++ app/src/main/java/com/tyagiabhinav/einvite/DB/DBHelper.java
@@ -0,0 +1,74 @@
+package com.tyagiabhinav.einvite.DB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by abhinavtyagi on 14/03/16.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "invite.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Create a table to hold invitation details.
+        final String SQL_CREATE_INVITATION_TABLE = "CREATE TABLE " + DBContract.InviteEntry.TABLE_NAME + " (" +
+                DBContract.InviteEntry.COL_ID + " TEXT PRIMARY KEY," +
+                DBContract.InviteEntry.COL_TITLE + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_TYPE + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_MESSAGE + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_TIME + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_DATE + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_WEBSITE + " TEXT, " +
+                DBContract.InviteEntry.COL_VENUE_NAME + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_VENUE_EMAIL + " TEXT, " +
+                DBContract.InviteEntry.COL_VENUE_CONTACT + " TEXT, " +
+                DBContract.InviteEntry.COL_VENUE_COUNTRY + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_VENUE_STATE + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_VENUE_ADD1 + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_VENUE_ADD2 + " TEXT, " +
+                DBContract.InviteEntry.COL_VENUE_CITY + " TEXT NOT NULL, " +
+                DBContract.InviteEntry.COL_VENUE_ZIP + " TEXT, " +
+                DBContract.InviteEntry.COL_VENUE_LATITUDE + " TEXT, " +
+                DBContract.InviteEntry.COL_VENUE_LONGITUDE + " TEXT, " +
+                DBContract.InviteEntry.COL_INVITEE + " TEXT NOT NULL" +
+                " );";
+
+        final String SQL_CREATE_USER_TABLE = "CREATE TABLE " + DBContract.UserEntry.TABLE_NAME + " (" +
+                DBContract.UserEntry.COL_USER_NAME + " TEXT NOT NULL, " +
+                DBContract.UserEntry.COL_USER_EMAIL + " TEXT NOT NULL PRIMARY KEY, " +
+                DBContract.UserEntry.COL_USER_CONTACT + " TEXT NOT NULL, " +
+                DBContract.UserEntry.COL_USER_COUNTRY + " TEXT NOT NULL, " +
+                DBContract.UserEntry.COL_USER_STATE + " TEXT NOT NULL, " +
+                DBContract.UserEntry.COL_USER_ADD1 + " TEXT NOT NULL, " +
+                DBContract.UserEntry.COL_USER_ADD2 + " TEXT, " +
+                DBContract.UserEntry.COL_USER_CITY + " TEXT NOT NULL, " +
+                DBContract.UserEntry.COL_USER_ZIP + " TEXT, " +
+                DBContract.UserEntry.COL_USER_TYPE + " TEXT NOT NULL" +
+                " );";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_INVITATION_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_USER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        // Note that this only fires if you change the version number for your database.
+        // It does NOT depend on the version number for your application.
+        // If you want to update the schema without wiping data, commenting out the next 2 lines
+        // should be your top priority before modifying this method.
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DBContract.InviteEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DBContract.UserEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/tyagiabhinav/einvite/DB/DBProvider.java
@@ -0,0 +1,231 @@
+package com.tyagiabhinav.einvite.DB;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by abhinavtyagi on 14/03/16.
+ */
+public class DBProvider extends ContentProvider {
+    private static final String LOG_TAG = DBProvider.class.getSimpleName();
+
+    private DBHelper mOpenHelper;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static final int INVITE = 100;
+    static final int INVITE_COLUMN = 101;
+    static final int USER = 300;
+    static final int USER_COLUMN = 301;
+
+    private static final SQLiteQueryBuilder sUserQueryBuilder;
+
+    static {
+        sUserQueryBuilder = new SQLiteQueryBuilder();
+
+        /// JOIN query for favorite movies...
+        // SELECT * FROM user INNER JOIN invite ON invite.invitee = user.email;
+        sUserQueryBuilder.setTables(
+                DBContract.UserEntry.TABLE_NAME + " INNER JOIN " +
+                        DBContract.InviteEntry.TABLE_NAME +
+                        " ON " + DBContract.UserEntry.TABLE_NAME +
+                        "." + DBContract.UserEntry.COL_USER_EMAIL +
+                        " = " + DBContract.InviteEntry.TABLE_NAME +
+                        "." + DBContract.InviteEntry.COL_INVITEE);
+    }
+
+    static UriMatcher buildUriMatcher() {
+
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = DBContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding code.
+        matcher.addURI(authority, DBContract.PATH_INVITATION, INVITE);
+        matcher.addURI(authority, DBContract.PATH_INVITATION + "/*", INVITE_COLUMN);
+        matcher.addURI(authority, DBContract.PATH_USER, USER);
+        matcher.addURI(authority, DBContract.PATH_USER + "/*", USER_COLUMN);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new DBHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Log.d(LOG_TAG, "query -->" + uri);
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+            // "invite/*"
+            case INVITE_COLUMN: {
+                Log.d(LOG_TAG, "INVITATION");
+                String inviteId = DBContract.InviteEntry.getInviteIdFromUri(uri);
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        DBContract.InviteEntry.TABLE_NAME + ", " + DBContract.UserEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        new String[]{inviteId},
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "user/*"
+            case USER_COLUMN: {
+                Log.d(LOG_TAG, "USER");
+                String id = DBContract.UserEntry.getUserIdFromUri(uri);
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        DBContract.UserEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        new String[]{id},
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "user"
+            case USER: {
+                Log.d(LOG_TAG, "USER");
+                retCursor = sUserQueryBuilder.query(mOpenHelper.getReadableDatabase(),
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                );
+                break;
+            }
+            // "invite"
+            case INVITE: {
+                Log.d(LOG_TAG, "INVITE");
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        DBContract.InviteEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        Log.d(LOG_TAG, "getType -->" + uri);
+        // Use the Uri Matcher to determine what kind of URI this is.
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            // Student: Uncomment and fill out these two cases
+            case INVITE:
+                return DBContract.InviteEntry.CONTENT_TYPE;
+            case INVITE_COLUMN:
+                return DBContract.InviteEntry.CONTENT_ITEM_TYPE;
+            case USER:
+                return DBContract.UserEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        Log.d(LOG_TAG, "insert");
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case INVITE: {
+                long _id = db.insertWithOnConflict(DBContract.InviteEntry.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+                if (_id > 0)
+                    returnUri = DBContract.InviteEntry.buildInviteUri();
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            case USER: {
+                long _id = db.insertWithOnConflict(DBContract.UserEntry.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+                if (_id > 0)
+                    returnUri = DBContract.UserEntry.buildUserUri();
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        Log.d(LOG_TAG, "delete");
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+        // this makes delete all rows return the number of rows deleted
+        if (null == selection) selection = "1";
+        switch (match) {
+            case INVITE:
+                rowsDeleted = db.delete(DBContract.InviteEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case USER:
+                rowsDeleted = db.delete(DBContract.UserEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // Because a null deletes all rows
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        Log.d(LOG_TAG, "update");
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match) {
+            case INVITE:
+                rowsUpdated = db.update(DBContract.InviteEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case USER:
+                rowsUpdated = db.update(DBContract.UserEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+}

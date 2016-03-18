a44975203773c325aebd27487e4e73e8e5da9dcb
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2016-03-18 09:24:19 +0530

    added request for permission for API23 for fine location

---
+++ app/src/main/java/com/tyagiabhinav/einvite/Network/ResponseReceiver.java
@@ -0,0 +1,33 @@
+package com.tyagiabhinav.einvite.Network;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+
+/**
+ * Created by abhinavtyagi on 13/03/16.
+ */
+@SuppressLint("ParcelCreator")
+public class ResponseReceiver extends ResultReceiver {
+    private Receiver mReceiver;
+
+    public ResponseReceiver(Handler handler) {
+        super(handler);
+    }
+
+    public void setReceiver(Receiver receiver) {
+        mReceiver = receiver;
+    }
+
+    public interface Receiver {
+        public void onReceiveResult(int resultCode, Bundle resultData);
+    }
+
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
+        if (mReceiver != null) {
+            mReceiver.onReceiveResult(resultCode, resultData);
+        }
+    }
+}
